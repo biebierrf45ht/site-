cfcfc484e5f09259066bede9b53787c7aace1182
biebierrf45ht <141328501+biebierrf45ht@users.noreply.github.com>
2024-05-24 13:44:32 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -2,22 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const registerForm = document.querySelector('.sign-up-container form');
     const loginForm = document.querySelector('.login-container form');
 
-    const usernameInputRegister = registerForm.querySelector('input[name="username"]');
-    const emailInputRegister = registerForm.querySelector('input[name="email"]');
-    const passwordInputRegister = registerForm.querySelector('input[name="password"]');
-    const emailInputLogin = loginForm.querySelector('input[name="email"]');
-    const passwordInputLogin = loginForm.querySelector('input[name="password"]');
-
     const handleSubmit = (form, endpoint, callback) => event => {
         event.preventDefault();
         const formData = new FormData(form);
         const formValues = Object.fromEntries(formData.entries());
-        const endpoint = '/register'; // Or '/login' depending
 
+        fetch(endpoint, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formValues)
         })
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur lors de la requête');
+            }
+            return response.text();
+        })
         .then(callback)
-        .catch(error => console.error('Erreur lors de la requête :', error));
+        .catch(error => {
+            console.error('Erreur lors de la requête :', error);
+        });
     };
 
     const handleRegisterResponse = message => {
@@ -30,6 +34,6 @@ document.addEventListener('DOMContentLoaded', function() {
         loginForm.reset();
     };
 
-    registerForm.addEventListener('submit', handleSubmit(registerForm, 'https://biebierrf45ht.github.io/site-/users.json', handleRegisterResponse));
-    loginForm.addEventListener('submit', handleSubmit(loginForm, 'https://biebierrf45ht.github.io/site-/users.json', handleLoginResponse));
+    registerForm.addEventListener('submit', handleSubmit(registerForm, '/register', handleRegisterResponse));
+    loginForm.addEventListener('submit', handleSubmit(loginForm, '/login', handleLoginResponse));
 });
